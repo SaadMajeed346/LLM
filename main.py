ac8eb83148c1f32405c36b5f689a766fc5c03856
SaadMajeed346 <saadmajeed346@gmail.com>
2025-05-26 09:38:43 +0500

    call main function by using agent

--- main.py
+++ main.py
@@ -6,31 +6,35 @@ from langchain_openai import ChatOpenAI
 
 import linkedin
 
-information = """
-Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman known for his leadership of Tesla, SpaceX, and X (formerly Twitter). Since 2025, he has been a senior advisor to United States president Donald Trump and the de facto head of the Department of Government Efficiency (DOGE). Musk has been considered the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion. He was named Time magazine's Person of the Year in 2021.
+from agents.linkedIn_lookup_agent import lookup as linkedin_lookup_agent
 
-Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada. He graduated from the University of Pennsylvania in the U.S. before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became a U.S. citizen.
+def agent_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = linkedin.scrape_linkedin_profile(
+        linkedin_profile_url= linkedin_username,
+    )
 
-In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence research but later left; growing discontent with the organization's direction in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes and rebranding it as X in 2023. In January 2025, he was appointed head of Trump's newly created DOGE. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017.
-"""
-if __name__ == "__main__":
-    load_dotenv()
-    print('Hello LangChain')
     summary_template = """
-        given the Linkedin information {information} about a person from I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-    """
+            given the Linkedin information {information} about a person from I want you to create:
+            1. a short summary
+            2. two interesting facts about them
+        """
 
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-4.1-nano")
 
-    chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = linkedin.scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/saad346/",
-        mock=True
-    )
+    chain = summary_prompt_template | llm
+
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Look up Agent Start Working")
+    agent_with(name="Muhammad Saad Majeed")
+
+
