f7dd2e711180782f9bf0754ce575ce6d07f59102
SaadMajeed346 <saadmajeed346@gmail.com>
2025-05-27 09:36:30 +0500

    add route

--- app.py
+++ app.py
@@ -3,3 +3,15 @@ from flask import Flask, render_template, request, jsonify
 from main import agent_with
 
 load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", debug=True)
