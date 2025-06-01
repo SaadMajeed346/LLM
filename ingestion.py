c47b5dbd54a38ec55a954268cd917b0e8f77ec4b
SaadMajeed346 <saadmajeed346@gmail.com>
2025-06-01 17:07:25 +0500

    update file to load data

--- ingestion.py
+++ ingestion.py
@@ -1,4 +1,10 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import  CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
 
 
+load_dotenv()
 if __name__ == "__main__":
     print(f"started")
